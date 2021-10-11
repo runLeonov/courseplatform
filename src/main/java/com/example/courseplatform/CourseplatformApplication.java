bd4dcbbdb7ead74c2d799ea6da28c17809456a39
runLeonov <leonovcasha@gmail.com>
2021-10-11 21:09:19 +0300

    first commit init

---
+++ src/main/java/com/example/courseplatform/CourseplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseplatformApplication.class, args);
+    }
+
+}
