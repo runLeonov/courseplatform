290e1df6c95366bb79e61da95b365a9c81042a8a
runLeonov <leonovcasha@gmail.com>
2021-11-04 14:41:42 +0200

    added logic for login
    (cherry picked from commit add51832a6b9e333cc5282834451956ce07dcc28)

--- src/main/java/com/example/courseplatform/controller/MainController.java
+++ src/main/java/com/example/courseplatform/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.example.courseplatform.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("")
 public class MainController {
 
-    @GetMapping("/main")
+    @RequestMapping("/main")
     public String getHomePage() {
         return "index.html";
     }
