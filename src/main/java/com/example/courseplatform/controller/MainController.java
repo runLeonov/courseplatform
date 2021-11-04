999ae5caa19c3c168c5bb501fbdf97861bed6915
runLeonov <leonovcasha@gmail.com>
2021-11-04 16:11:55 +0200

    developSasha changed login logic

--- src/main/java/com/example/courseplatform/controller/MainController.java
+++ src/main/java/com/example/courseplatform/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.example.courseplatform.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("")
 public class MainController {
 
-    @RequestMapping("/main")
+    @GetMapping("/main")
     public String getHomePage() {
         return "index.html";
     }
