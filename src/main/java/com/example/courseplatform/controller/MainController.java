fd1966a60c97eeca617a1ebb343f91a7736cbc01
runLeonov <leonovcasha@gmail.com>
2021-11-08 17:57:03 +0200

    refactored for valid nextlesson

--- src/main/java/com/example/courseplatform/controller/MainController.java
+++ src/main/java/com/example/courseplatform/controller/MainController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
@@ -16,11 +16,15 @@ import javax.servlet.http.HttpSession;
 public class MainController {
     @Autowired
     UserService userService;
-    @GetMapping(value = {"/main", "/"})
-    public String getHomePage(HttpSession session) {
-        User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        session.setAttribute("username", principal.getUsername());
-        session.setAttribute("lesson10", userService.getTestList(principal.getUsername()));
+
+    @GetMapping(value = {"/main", "/*"})
+    public String getHomePage(
+            @RequestParam("lessonNumber") Integer lessonNumber,
+            HttpSession session, Model model) {
+        User user = getUserFromSession();
+        session.setAttribute("userDB", user);
+        session.setAttribute("lesson10", userService.getTestList(user.getUsername()));
+        model.addAttribute("thisLessonGrade", user.getLessonTestByLessonNumber(lessonNumber));
         return "main.html";
     }
 
@@ -28,14 +32,22 @@ public class MainController {
     public String getTestValues(
             @RequestParam("testsSummary") Integer testsSummary,
             @RequestParam("lessonNumber") Integer lessonNumber,
-            @RequestParam("nextLesson") String nextLesson
+            @RequestParam("nextLesson") String nextLesson,
+            Model model, HttpSession session
     ) {
+        User user = getUserFromSession();
+        session.setAttribute("userDB", user);
+        model.addAttribute("thisLessonGrade", user.getLessonTestByLessonNumber(lessonNumber));
+        userService.setTestGrade(user.getUsername(), testsSummary, lessonNumber);
+        return nextLesson + ".html";
+    }
+
+    private User getUserFromSession() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof UserDetails) {
-            String username = ((UserDetails)principal).getUsername();
-            userService.setTestGrade(username, testsSummary, lessonNumber);
+            return  (User) principal;
         }
-        return nextLesson + ".html";
+        return null;
     }
 
 }
